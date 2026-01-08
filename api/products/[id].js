ec2840248997b62a2f08ccb0de0be17cb54eb6b0
Cold9091 <angel923383808@gmail.com>
2026-01-08 00:17:14 +0000

    Atualizar ambos arquivos de produtos (api/products.js e api/products/[id].js) com novas imagens e vídeos

--- api/products/[id].js
+++ api/products/[id].js
@@ -15,7 +15,13 @@ export default function handler(req, res) {
         "https://res.cloudinary.com/dl90hjhoj/image/upload/v1765880925/WhatsApp_Image_2025-12-15_at_14.03.13_1_tyaq0u.jpg",
         "https://res.cloudinary.com/dl90hjhoj/image/upload/v1765880926/WhatsApp_Image_2025-12-15_at_14.03.14_lgyja8.jpg",
         "https://res.cloudinary.com/dl90hjhoj/image/upload/v1765880925/WhatsApp_Image_2025-12-15_at_14.03.13_qnlgtc.jpg",
-        "https://res.cloudinary.com/dl90hjhoj/image/upload/v1765880923/WhatsApp_Image_2025-12-15_at_14.03.16_1_uil3iq.jpg"
+        "https://res.cloudinary.com/dl90hjhoj/image/upload/v1765880923/WhatsApp_Image_2025-12-15_at_14.03.16_1_uil3iq.jpg",
+        "https://res.cloudinary.com/dl90hjhoj/image/upload/v1767828479/WhatsApp_Image_2026-01-07_at_23.33.43_1_mmjauy.jpg",
+        "https://res.cloudinary.com/dl90hjhoj/image/upload/v1767828479/WhatsApp_Image_2026-01-07_at_23.33.42_xbnpqz.jpg",
+        "https://res.cloudinary.com/dl90hjhoj/image/upload/v1767828479/WhatsApp_Image_2026-01-07_at_23.33.43_w4qlmh.jpg",
+        "https://res.cloudinary.com/dl90hjhoj/image/upload/v1767828479/WhatsApp_Image_2026-01-07_at_23.33.42_1_bqgxev.jpg",
+        "https://res.cloudinary.com/dl90hjhoj/image/upload/v1767828503/WhatsApp_Image_2026-01-07_at_23.33.45_1_glsbvn.jpg",
+        "https://res.cloudinary.com/dl90hjhoj/image/upload/v1767828503/WhatsApp_Image_2026-01-07_at_23.33.46_ycjhm7.jpg"
       ],
       video: null,
       videos: null,
@@ -41,7 +47,8 @@ export default function handler(req, res) {
       video: "https://res.cloudinary.com/dl90hjhoj/video/upload/v1765880479/WhatsApp_Video_2025-12-15_at_13.29.15_wg9bjf.mp4",
       videos: [
         "https://res.cloudinary.com/dl90hjhoj/video/upload/v1765881413/WhatsApp_Video_2025-12-15_at_13.25.14_ptughb.mp4",
-        "https://res.cloudinary.com/dl90hjhoj/video/upload/v1765881417/WhatsApp_Video_2025-12-15_at_13.25.11_acwhrj.mp4"
+        "https://res.cloudinary.com/dl90hjhoj/video/upload/v1765881417/WhatsApp_Video_2025-12-15_at_13.25.11_acwhrj.mp4",
+        "https://res.cloudinary.com/dl90hjhoj/video/upload/v1767828135/WhatsApp_Video_2026-01-07_at_23.33.51_xv2osl.mp4"
       ],
       category: "organizacao",
       featured: true,
